f87236147d5194485d373a400647f3d389241a58
Matthew Weier O'Phinney <matthew@zend.com>
2016-08-16 11:35:35 -0500

    Modernize module
    - Removed root package `Module.php` file; it's essentially unnecessary, and
      folks can add autoloading to their application manually if not using Composer.
    - Use short array notation, and split multiple entries over multiple lines in
      the test bootstrap.
    - Added phpcs configuration, and bumped minimum required version to 2.6.2
    - Set up composer scripts for QA tools.
    - Set up a modern test matrix for Travis CI.
      - lowest/locked/latest testing strategy, to pick up when a change may not be
        backwards and/or forwards compatible
      - use docker builds
      - cache dependencies between builds
      - reference composer scripts, so that the build configuration may not
        necessarily need to change if QA tools change
    - Bump dependencies to versions known to be stable and forwards compatible with
      v3 releases.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -20,12 +20,14 @@ use ZfrCorsTest\Util\ServiceManagerFactory;
 
 ini_set('error_reporting', E_ALL);
 
-$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+$files = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+];
 
 foreach ($files as $file) {
     if (file_exists($file)) {
         $loader = require $file;
-
         break;
     }
 }
@@ -34,14 +36,14 @@ if (! isset($loader)) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you install via composer?');
 }
 
-$loader->add('ZfrCorsTest\\', __DIR__);
-
-$configFiles = array(__DIR__ . '/TestConfiguration.php', __DIR__ . '/TestConfiguration.php.dist');
+$configFiles = [
+    __DIR__ . '/TestConfiguration.php',
+    __DIR__ . '/TestConfiguration.php.dist',
+];
 
 foreach ($configFiles as $configFile) {
     if (file_exists($configFile)) {
         $config = require $configFile;
-
         break;
     }
 }
